94d7d47eb31b206b9869dd5bb16e06e449a1eda5
Odysseus7 <chalitandunu130@gmail.com>
2021-05-19 15:56:21 +0200

    setup firestore cdn

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const db = firebase.firestore();
+
+
+document.querySelector(".form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const remark = document.querySelector(".form__remark").value;
+    const number = document.querySelector(".form__number").value;
+    
+
+});
