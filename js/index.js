8d058c376205c997d2e699f55e60f8647edcd4c7
Odysseus7 <chalitandunu130@gmail.com>
2021-05-19 16:04:39 +0200

    Implement adding remarks to firestore

--- js/index.js
+++ js/index.js
@@ -3,8 +3,18 @@ const db = firebase.firestore();
 
 document.querySelector(".form").addEventListener("submit", (event) => {
     event.preventDefault();
-    const remark = document.querySelector(".form__remark").value;
     const number = document.querySelector(".form__number").value;
+    const remark = document.querySelector(".form__remark").value;
     
-
+    
+    db.collection("remarks").add({
+        number,
+        remark,
+    })
+    .then((docRef) => {
+        window.alert("Remark succesfully posted");
+    })
+    .catch((error) => {
+        window.alert("Something went wrong.");
+    });
 });
